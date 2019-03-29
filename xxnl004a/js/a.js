c9750dd2cd83418615020875eec7e86460b6833f
yinyina <49050734+yinyina@users.noreply.github.com>
2019-03-29 16:27:25 +0800

    Add files via upload

---
+++ xxnl004a/js/a.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+//课程代码：
+var CourseCode="xxnl004a"
+var VideoFolder="xxjscourse"
+var sTitle="校本研修"
+
+//一级栏目名称：
+var arrMyMenu=["教师说课评价标准-zta","教师说课评价表-ztb","简易多媒体环境下教师教学评价标准-ztc","简易多媒体环境下教师听课评价表-ztd"];
+
+//一级栏目类型，没有子栏目时填写，视频栏目填video,文本栏目填swf
+var arrStype=["swf","swf","swf","swf","video"]
+
+//依次为各个栏目的二级栏目名称数组
+var arrMyChildName1
+var arrMyChildName2
+var arrMyChildName3
+var arrMyChildName4
+var arrMyChildName5
+
+
+//依次为各二级栏目的类型
+var arrMyChildStype1
+var arrMyChildStype2
+var arrMyChildStype3
+var arrMyChildStype4
+var arrMyChildStype5
