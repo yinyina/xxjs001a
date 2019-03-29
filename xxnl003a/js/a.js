4f19fa3859f64b04536cc62a5948a648106bf030
yinyina <49050734+yinyina@users.noreply.github.com>
2019-03-29 16:23:02 +0800

    Add files via upload

---
+++ xxnl003a/js/a.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+//课程代码：
+var CourseCode="xxnl003a"
+var VideoFolder="xxjscourse"
+var sTitle="解析简易多媒体环境下的小学语文教学案例"
+var arrVidzta=["C72E3A6963D0B6C59C33DC5901307461","","6872FB84B3BD032F9C33DC5901307461","BE1D63DB758228BA9C33DC5901307461","","4515D758322A45D79C33DC5901307461"];
+var arrVidztb=["38C9B54FABD7E4039C33DC5901307461","","A214C10D26FE38FB9C33DC5901307461","E10037C6BE4701D49C33DC5901307461"];
+var arrVidztc=["57664C50F6ED29FA9C33DC5901307461","","811C7BDC03573B349C33DC5901307461","FAB49CB7221196F39C33DC5901307461"];
+
+
+
+//一级栏目名称：
+var arrMyMenu=["主题一 简易多媒体环境下的古诗文阅读教学-zta","主题二 简易多媒体环境下的现代作品阅读教学-ztb","主题三 简易多媒体环境下的写作教学-ztc"];
+
+//一级栏目类型，没有子栏目时填写，视频栏目填video,文本栏目填swf
+var arrStype=["video","video","swf"]
+
+//依次为各个栏目的二级栏目名称数组
+var arrMyChildName1=["视频案例：《伯牙绝弦》","文本资源：《伯牙绝弦》教学设计","微视频：《伯牙绝弦》教学设计分析","视频案例：《卖油翁》","作业：简易多媒体环境下的媒体应用分析表","讲授微视频：如何评定简易多媒体环境下讲授为主的课堂教学"]
+var arrMyChildName2=["视频案例：《玲玲的画》","文本资源：《玲玲的画》教学设计","微视频：《玲玲的画》教学设计分析","视频案例：《九寨沟》","作业：简易多媒体环境下的课堂教学分析表"]
+var arrMyChildName3=["视频案例：《假如我会变》","文本资源：《假如我会变》教学设计","微视频：《假如我会变》教学设计分析","视频案例：《给老师的一封信》","文本资源：简易多媒体环境下的媒体应用分析表","教学设计《散步》修改批注样例"]
+var arrMyChildName4
+var arrMyChildName5
+
+
+//依次为各二级栏目的类型
+var arrMyChildStype1=["video","swf","video","video","swf","video"]
+var arrMyChildStype2=["video","swf","video","video","swf"]
+var arrMyChildStype3=["video","swf","video","video","swf","swf"]
+var arrMyChildStype4
+var arrMyChildStype5
