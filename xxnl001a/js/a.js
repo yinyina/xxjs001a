13fa587e8044783b3ef79c19028d6b4df43d3923
yinyina <49050734+yinyina@users.noreply.github.com>
2019-03-29 15:58:48 +0800

    Add files via upload

---
+++ xxnl001a/js/a.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+//课程代码：
+var CourseCode="xxnl001a"
+var VideoFolder="xxjscourse"
+var sTitle="技术支持的讲授为主的教学"
+var arrVidzta=["E256E9C4CDEBE28F9C33DC5901307461"];
+var arrVidztb=["C18F18B60C7FDE159C33DC5901307461"];
+var arrVidztc=["36D0188520DA38419C33DC5901307461"];
+var arrVidztd=["1265DF8B9D1C1BD89C33DC5901307461"];
+var arrVidzte=["57CD5455FB4D0B6E9C33DC5901307461","4A12A8A9E528E73B9C33DC5901307461"];
+
+//一级栏目名称：
+var arrMyMenu=["主题一 讲授教学概述-zta","主题二 讲授教学的理论基础-ztb","主题三 讲授教学的基本程序-ztc","主题四 讲授教学应该注意的问题-ztd","主题五 技术支持的讲授教学-zte"];
+
+//一级栏目类型，没有子栏目时填写，视频栏目填video,文本栏目填swf
+var arrStype=["video","video","swf","swf","video"]
+
+//依次为各个栏目的二级栏目名称数组
+var arrMyChildName1=["微课：讲授教学概念和基本形式","文献资料：提高讲授教学有效性的基本策略","文献资料：浅谈初中教师课堂讲授技能策略"]
+var arrMyChildName2=["微课：讲授教学的理论基础","文献资料：奥苏贝尔学习理论在中学讲授教学中的启示","文献资料：论讲授教学法之合理内核"]
+var arrMyChildName3=["微课：讲授教学的基本程序","文献资料：有效控制讲授环节全面提高教学质量"]
+var arrMyChildName4=["微课：讲授教学应该注意的问题","阅读文献：专家关于新课程理念下的讲授法的精彩论述","文献资料：对新课改中讲授教学的再认识"]
+var arrMyChildName5=["讲授微视频：技术对讲授教学的支持","讲授微视频：创设情境，支持讲授","文献资料:谈信息技术在课堂讲授教学中的应用"]
+
+
+//依次为各二级栏目的类型
+var arrMyChildStype1=["video","swf","swf"]
+var arrMyChildStype2=["video","swf","swf"]
+var arrMyChildStype3=["video","swf"]
+var arrMyChildStype4=["video","swf","swf"]
+var arrMyChildStype5=["video","video","swf"]
